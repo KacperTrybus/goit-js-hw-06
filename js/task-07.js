e9ad9f7abcf5742e8ea8cb60d3d0edb5cde8d0a3
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2024-01-30 16:15:36 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,3 @@
-/*Napisz skrypt, który reaguje na zmianę wartości elementu input#font-size-control
-(zdarzenie input) i zmienia styl inline span#text aktualizując właściwość font-size.
-W rezultacie, podczas zmiany wartości suwakiem,
-ma zmieniać się rozmiar tekstu w elemencie span */
 
 const inputText = document.getElementById("text"); //span
 const inputSlide = document.getElementById("font-size-control"); //input
